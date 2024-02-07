80b3f849d92d7bbae4133af80ef68f90243063d3
ali9ahmad <temur.khan.dispatch@gmail.com>
2024-02-07 10:42:28 +0500

    Revert "interface for login is completed"
    This reverts commit 21a78d3a2be964564def3d0bdf52f4db0c1d9634.

--- src/components/UserLoginDetails.jsx
+++ src/components/UserLoginDetails.jsx
@@ -1,44 +1,25 @@
-import { useState } from 'react';
+import React from 'react';
 import {
   FormControl,
   FormLabel,
   FormErrorMessage,
+  FormHelperText,
   Input,
   Heading,
   Box,
+  Grid,
+  GridItem,
   Flex,
   Button,
   Text,
-  Checkbox,
-  Link,
 } from '@chakra-ui/react';
 
 const UserLoginDetails = () => {
-  const [values, setValues] = useState({
-    email: '',
-    password: '',
-  });
+  const [input, setInput] = useState('');
 
-  const handleEmailInput = (e) => {
-    setValues({ ...values, email: e.target.value });
-  };
+  const handleEmailInput = (e) => setInput(e.target.value);
 
-  const handlePasswordInput = (e) => {
-    setValues({ ...values, password: e.target.value });
-  };
-
-  const [submitted, setSubmitted] = useState(false);
-  const [valid, setValid] = useState(false);
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (values.email && values.password) {
-      setValid(true);
-    }
-    setSubmitted(true);
-  };
-
-  const isError = valid === false && submitted === true;
+  const isError = input === '';
 
   return (
     <>
@@ -46,76 +27,71 @@ const UserLoginDetails = () => {
         <Heading as="h1" mb="45px" color="blue.900" fontSize="40px">
           Вход в платформу
         </Heading>
-        <form onSubmit={handleSubmit}>
-          <FormControl isRequired color="black" isInvalid={isError}>
-            <FormLabel fontSize="20px">Email или номер телефона</FormLabel>
-            <Input
-              type="email"
-              onChange={handleEmailInput}
-              borderColor="gray.300"
-              w="100%"
-              h="55px"
-              borderRadius="10px"
-              _hover={{ borderColor: 'blue.300' }}
-            />
-            {isError ? (
-              <FormErrorMessage>Неверный Email или номер телефона</FormErrorMessage>
-            ) : null}
-          </FormControl>
-
-          <FormControl isRequired color="black" mt="25px" isInvalid={isError}>
-            <FormLabel fontSize="20px">Пароль</FormLabel>
-            <Input
-              type="password"
-              borderColor="gray.300"
-              w="100%"
-              h="55px"
-              _hover={{ borderColor: 'blue.300' }}
-            />
-            {isError ? <FormErrorMessage>Неверный пароль</FormErrorMessage> : null}
-          </FormControl>
-
-          <Flex justify="space-between" mt="30px">
-            <FormControl display="flex" alignItems="center" color="black">
-              <Checkbox name="rememberMe" size="lg" colorScheme="blue" borderColor="gray.300" />
-              <FormLabel mb="0" ml="15px">
-                Запомнить меня
-              </FormLabel>
-            </FormControl>
-            <Link
-              color="blue.500"
-              href="#"
-              w="170px"
-              fontWeight="semibold"
-              textDecoration="underline"
-              textDecorationSkipInk="15px"
-              _hover={{ color: 'blue.600' }}>
-              Забыли пароль?
-            </Link>
-          </Flex>
-
-          <Button
-            type="submit"
-            bg="blue.500"
-            h="55px"
-            _hover={{ color: 'white.900', boxShadow: 'lg' }}
-            mt="50px"
-            w="100%"
-            color="white">
-            Войти
-          </Button>
-
-          <Button
-            variant="outline"
+        <FormControl isRequired color="black" isInvalid={isError}>
+          <FormLabel fontSize="20px">Email или номер телефона</FormLabel>
+          <Input
+            type="email"
+            value={input}
+            onChange={handleEmailInput}
             borderColor="gray.300"
-            h="55px"
-            _hover={{ borderColor: 'blue.300', boxShadow: 'md' }}
-            mt="20px"
             w="100%"
-            color="blue.500">
-            Зарегистрироваться
-          </Button>
-        </form>
+            h="55px"
+            borderRadius="10px"
+            _hover={{ borderColor: 'blue.300' }}
+          />
+          {isError ? <FormErrorMessage>Неверный Email или номер телефона</FormErrorMessage> : null}
+        </FormControl>
+        <FormControl isRequired color="black" mt="25px">
+          <FormLabel fontSize="20px">Пароль</FormLabel>
+          <Input
+            type="password"
+            borderColor="gray.300"
+            w="100%"
+            h="55px"
+            _hover={{ borderColor: 'blue.300' }}
+          />
+          {<FormErrorMessage>Неверный пароль</FormErrorMessage>}
+        </FormControl>
+
+        <Flex justify="space-between" mt="30px">
+          <FormControl display="flex" alignItems="center" color="black">
+            <Checkbox name="rememberMe" size="lg" colorScheme="blue" borderColor="gray.300" />
+            <FormLabel mb="0" ml="15px">
+              Запомнить меня
+            </FormLabel>
+          </FormControl>
+          <Link
+            color="blue.500"
+            href="#"
+            w="170px"
+            fontWeight="semibold"
+            textDecoration="underline"
+            textDecorationSkipInk="15px"
+            _hover={{ color: 'blue.600' }}>
+            Забыли пароль?
+          </Link>
+        </Flex>
+
+        <Button
+          bg="blue.500"
+          h="55px"
+          _hover={{ color: 'white.900', boxShadow: 'lg' }}
+          mt="50px"
+          w="100%"
+          color="white">
+          Войти
+        </Button>
+        <Button
+          variant="outline"
+          borderColor="gray.300"
+          h="55px"
+          _hover={{ borderColor: 'blue.300', boxShadow: 'md' }}
+          mt="20px"
+          w="100%"
+          color="blue.500">
+          Зарегистрироваться
+        </Button>
+
         <Box position="fixed" bottom={6} left={240} width="100%" textAlign="center" py={2}>
           <Text color="gray.500" alignSelf="bottom" fontWeight="semibold">
             Copyright © URecruit. Все права защищены
