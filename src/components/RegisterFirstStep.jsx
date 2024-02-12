825d99ea073aeffd0ebcee20919e5352ba6c75c6
ali9ahmad <temur.khan.dispatch@gmail.com>
2024-02-12 14:32:47 +0500

    login 1st stap is in process

--- src/components/RegisterFirstStep.jsx
+++ src/components/RegisterFirstStep.jsx
@@ -44,89 +44,85 @@ export default function RegisterFirstStep() {
   };
 
   return (
-    <Box w="800px" mx="auto">
-      <Heading as="h1" mb="45px" color="blue.900" fontSize="40px">
-        Registration
-      </Heading>
+    <Box w={{ sm: 200, md: 400, lg: 700 }} mx="auto">
+      <Box w="60%" mx="auto">
+        <Heading as="h1" mb="45px" color="blue.900" fontSize="40px">
+          Registration
+        </Heading>
 
-      <form onSubmit={handleSubmit(onSubmit)} noValidate>
-        <FormControl isInvalid={errors.name} isRequired color="black" mt="25px">
-          <FormLabel htmlFor="name" fontSize="20px">
-            Inter Your Full Name
-          </FormLabel>
-          <InputGroup size="lg">
-            <InputLeftElement
-              pointerEvents="none"
-              children={<Icon as={VscAccount} color="gray.400" />}
-              size="xl"
-            />
-            <Input
-              type="name"
-              id="name"
-              w="100%"
-              borderColor="gray.300"
-              borderRadius="10px"
-              placeholder="Name (Middle Name) Surname"
-              _placeholder={{ color: 'gray.400' }}
-              _hover={{ borderColor: 'blue.300' }}
-              {...register('name', {
-                required: 'This is required',
-              })}
-            />
-          </InputGroup>
-          <FormErrorMessage>{errors.name && errors.name.message}</FormErrorMessage>
-        </FormControl>
+        <form onSubmit={handleSubmit(onSubmit)} noValidate>
+          <FormControl isInvalid={errors.name} isRequired color="black" mt="25px">
+            <FormLabel htmlFor="name" fontSize="20px">
+              Inter Your Full Name
+            </FormLabel>
+            <InputGroup>
+              <InputLeftElement
+                pointerEvents="none"
+                children={<Icon as={VscAccount} color="gray.400" />}
+              />
+              <Input
+                type="name"
+                id="name"
+                w="100%"
+                borderColor="gray.300"
+                borderRadius="10px"
+                placeholder="Name (Middle Name) Surname"
+                _placeholder={{ color: 'gray.400' }}
+                _hover={{ borderColor: 'blue.300' }}
+                {...register('name', {
+                  required: 'This is required',
+                })}
+              />
+            </InputGroup>
+            <FormErrorMessage>{errors.name && errors.name.message}</FormErrorMessage>
+          </FormControl>
 
-        <FormControl isInvalid={errors.email} isRequired color="black" mt="20px">
-          <FormLabel htmlFor="email" fontSize="20px" color="black">
-            Enter your Email Address
-          </FormLabel>
-          <InputGroup size="lg">
-            <InputLeftElement
-              pointerEvents="none"
-              children={<EmailIcon color="gray.400" />}
-              size="xl"
-            />
-            <Input
-              type="email"
-              id="email"
-              placeholder="email@example.com"
-              _placeholder={{ color: 'gray.400' }}
-              w="100%"
-              borderColor="gray.300"
-              borderRadius="10px"
-              _hover={{ borderColor: 'blue.300' }}
-              {...register('email', {
-                required: 'Email address is required',
-                pattern: {
-                  value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-                  message: 'Invalid email format',
-                },
-              })}
-            />
-          </InputGroup>
-          <FormErrorMessage>{errors.email && errors.email.message}</FormErrorMessage>
-        </FormControl>
+          <FormControl isInvalid={errors.email} isRequired color="black" mt="20px">
+            <FormLabel htmlFor="email" fontSize="20px" color="black">
+              Enter your Email Address
+            </FormLabel>
+            <InputGroup>
+              <InputLeftElement pointerEvents="none" children={<EmailIcon color="gray.400" />} />
+              <Input
+                type="email"
+                id="email"
+                placeholder="email@example.com"
+                _placeholder={{ color: 'gray.400' }}
+                w="100%"
+                borderColor="gray.300"
+                borderRadius="10px"
+                _hover={{ borderColor: 'blue.300' }}
+                {...register('email', {
+                  required: 'Email address is required',
+                  pattern: {
+                    value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                    message: 'Invalid email format',
+                  },
+                })}
+              />
+            </InputGroup>
+            <FormErrorMessage>{errors.email && errors.email.message}</FormErrorMessage>
+          </FormControl>
 
-        <Button
-          disabled={!isDirty || !isValid}
-          bg="gray.300"
-          color="black"
-          size="lg"
-          _hover={{ boxShadow: 'lg' }}
-          mt="50px"
-          w="100%"
-          type="submit">
-          Login
-        </Button>
-      </form>
+          <Button
+            disabled={!isDirty || !isValid}
+            bg="gray.300"
+            color="black"
+            _hover={{ boxShadow: 'lg' }}
+            mt="35px"
+            w="100%"
+            type="submit">
+            Login
+          </Button>
+        </form>
 
-      <DevTool control={control} />
+        <DevTool control={control} />
 
-      <Box position="fixed" bottom={6} left={240} width="100%" textAlign="center" py={2}>
-        <Text color="gray.500" alignSelf="bottom" fontWeight="semibold">
-          Copyright © URecruit. All rights are reserved.
-        </Text>
+        <Box position="fixed" bottom={6} left={240} width="100%" textAlign="center" py={2}>
+          <Text color="gray.500" alignSelf="bottom" fontWeight="semibold">
+            Copyright © URecruit. All rights are reserved.
+          </Text>
+        </Box>
       </Box>
     </Box>
   );
